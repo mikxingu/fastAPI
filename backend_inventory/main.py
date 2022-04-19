2b1e963f035c6075641eec8bfefd85824f4be4a2
Mike <michelalvs@gmail.com>
2022-04-19 20:35:22 -0300

    Added Crud operations for inventory backend

--- backend_inventory/main.py
+++ backend_inventory/main.py
@@ -1,15 +1,66 @@
+from itertools import product
 from typing import Optional
 
 from fastapi import FastAPI
+import fastapi
+from fastapi.middleware.cors import CORSMiddleware
+
+from redis_om import get_redis_connection, HashModel
 
 app = FastAPI()
 
 
+#Libera consumo para o frontend comunicar com nossa aplicação
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+    
+)
+
+redis = get_redis_connection(
+    host="redis-13847.c56.east-us.azure.cloud.redislabs.com",
+    port=13847,
+    password="ltV5q4LNdbfM5KC0susNntvcm2oNnr3I",
+    decode_responses=True
+)
+
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+    
+    class Meta:
+        database = redis
+
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Fast API Crash Course"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get('/products')
+def all():
+    return [format(pk) for pk in Product.all_pks()]
+
+def format(pk: str):
+    product = Product.get(pk)
+    
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
+@app.get('/products/{pk}')
+def get(pk: str):
+    return Product.get(pk)
+
+@app.delete('/products/{pk}')
+def delete(pk: str):
+    return Product.delete(pk)
+
+@app.post('/products')
+def create(product: Product):
+    return product.save()
